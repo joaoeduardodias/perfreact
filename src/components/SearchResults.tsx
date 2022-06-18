8d7c90e639912400e29538a74f49a05a943c1776
joaoeduardodias <joaoeduardodias123@gmail.com>
2022-06-18 09:29:13 -0300

    feat: create project of study performance in react

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,20 @@
+import { ProductItem } from './ProductItem'
+
+interface SearchResultsProps {
+  results: Array<{
+    id: number
+    price: number
+    title: string
+  }>
+}
+
+export function SearchResults({ results }: SearchResultsProps) {
+  return (
+    <div>
+      {results.map((product) => {
+        // eslint-disable-next-line react/jsx-key
+        return <ProductItem key={product.id} product={product} />
+      })}
+    </div>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app'
-import '../styles/globals.scss'
 
 const MyApp = function ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
